ee5cfe40bb6580c7b032dffb6064087f2f17b369
Adam Schuber <adamschuber@outlook.com>
2022-03-29 12:31:41 +0200

    lab02 done!

--- src/standalone/lab02/about-main.c
+++ src/standalone/lab02/about-main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
+    int total_size = 0;
+    for (int i = 0; i < argc; ++i)
+    {
+        total_size += strlen(argv[i]);
+        printf("%-20s%d\n", argv[i], strlen(argv[i]));
+    }
+    double average = total_size / argc;
+    printf("%-20s%d\n", "Total length", total_size);
+    printf("%-20s%.2f\n", "Average length", average);
+    return total_size;
 }
+
+
